73d7c98da8cf6c1c1332e8f9cd3417a1b935385d
Timmy Forde <G00403502@atu.ie>
2023-10-20 09:34:16 +0000

    final commit

--- src/componets/create.js
+++ src/componets/create.js
@@ -1,9 +1,52 @@
+import { useState } from "react";
 function Create() {
-    return (
-      <div>
-        <h1>Hello From create Componets</h1>
-      </div>
-    );
+
+  const [title, setTitle] = useState('');
+  const [cover, setCover] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleSubmit =(e)=>{
+    e.preventDefault()
+
+    console.log('Title: '+title+ 
+    'Cover '+cover+
+    'Author '+author); //printing the inputed deatils to the console
   }
-  
-  export default Create;
+  return (
+    <div>
+      <h1>Hello From create Componets</h1>
+      {/* form to hold all the information so the user can enter in the details */}
+      <form onSubmit={handleSubmit}> 
+        <div className="form-group">
+          <label>Edit Book Title: </label>
+          <input type="text"
+            className="form-control"
+            value={title}
+            onChange={(e) => { setTitle(e.target.value) }}
+          />
+        </div>
+        <div className="form-group">
+          <label>Edit Book Author: </label>
+          <input type="text"
+            className="form-control"
+            value={cover}
+            onChange={(e) => { setCover(e.target.value) }}
+          />
+        </div>
+        <div className="form-group">
+          <label>Edit Front Page Url: </label>
+          <input type="text"
+            className="form-control"
+            value={author}
+            onChange={(e) => { setAuthor(e.target.value) }}
+          />
+        </div>
+        <div>
+         <input type='submit' value='Add Book'></input>
+         </div>
+      </form>
+    </div>
+  );
+}
+
+export default Create;
--- src/componets/read.js
+++ src/componets/read.js
@@ -1,52 +1,36 @@
+import { useEffect, useState } from "react";
 import Books from "./books";
+import axios from "axios"; //http client (will talk to the web)
 function Read() {
+
+    const [data, setData] = useState([]);
+
+
     // Adding all the book information 
-    const data = [
-        {
-            "title": "Learn Git in a Month of Lunches",
-            "isbn": "1617292419",
-            "pageCount": 0,
-            "thumbnailUrl":
-                "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/umali.jpg",
-            "status": "MEAP",
-            "authors": ["Rick Umali"],
-            "categories": []
-        },
-        {
-            "title": "MongoDB in Action, Second Edition",
-            "isbn": "1617291609",
-            "pageCount": 0,
-            "thumbnailUrl":
-                "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/banker2.jpg",
-            "status": "MEAP",
-            "authors": [
-                "Kyle Banker",
-                "Peter Bakkum",
-                "Tim Hawkins",
-                "Shaun Verch",
-                "Douglas Garrett"
-            ],
-            "categories": []
-        },
-        {
-            "title": "Getting MEAN with Mongo, Express, Angular, and Node",
-            "isbn": "1617292036",
-            "pageCount": 0,
-            "thumbnailUrl":
-                "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/sholmes.jpg",
-            "status": "MEAP",
-            "authors": ["Simon Holmes"],
-            "categories": []
-        }
-    ]
+    useEffect( // using react hook
+        () => {//get method calling using asysnc
+            axios.get('https://jsonblob.com/api/jsonblob/1161593332966481920')
+                .then(
+                    (response) => {
+                        setData(response.data.books);// pulling out the data from the array called books
+                    }
+                )
+                //will catchany errors
+                .catch(
+                    (error) => {// Arrow function 
+                        console.log(error); // call back function 
+                    } // print out "Error" to console if there is one
+                )
+        }, []
+    );
 
     return (
-      <div>
-        <h1>Hello From Read Componets</h1>
-        <Books myBooks={data}></Books>
-      </div>
-   
+        <div>
+            <h1>Hello From Read Componets</h1>
+            <Books myBooks={data}></Books>
+        </div>
+
     );
-  }
-  
-  export default Read;
+}
+
+export default Read;
